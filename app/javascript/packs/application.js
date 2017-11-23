206834e58f6718a359a1003400576e5107a3a51d
Guillaume Hercot <guillaume.hercot@gmail.com>
2017-11-23 21:09:08 +0100

    List of ingredients displaying when hovering on card

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,4 +7,20 @@
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
 
-console.log('Hello World from Webpacker')
+
+const cards = document.querySelectorAll(".card");
+cards.forEach((card) => {
+  const ingredients = card.querySelectorAll("p");
+  card.addEventListener("mouseenter", (event) => {
+    ingredients.forEach((ingredient) => {
+      ingredient.classList.remove("hidden");
+    });
+    event.currentTarget.classList.add("card-ingredients");
+  });
+  card.addEventListener("mouseleave", (event) => {
+    ingredients.forEach((ingredient) => {
+      ingredient.classList.add("hidden");
+    });
+    event.currentTarget.classList.remove("card-ingredients");
+  });
+});
